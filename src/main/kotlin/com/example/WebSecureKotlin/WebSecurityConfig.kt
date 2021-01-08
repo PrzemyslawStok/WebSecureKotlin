7ae0ec1c1af834f28a839000c96578f2580591e9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-08 19:32:46 +0100

    cd..

--- src/main/kotlin/com/example/WebSecureKotlin/WebSecurityConfig.kt
+++ src/main/kotlin/com/example/WebSecureKotlin/WebSecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
@@ -22,6 +23,20 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .and()
                 .logout()
                 .permitAll()
+        }
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+        auth?.let{
+            it.inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN","USER")
         }
     }
 }
