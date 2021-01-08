c0afbe1b8ee915e20cc64907e46cbd4bb4379abb
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-08 18:14:25 +0100

    cd..

---
+++ src/test/kotlin/com/example/WebSecureKotlin/WebSecureKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.WebSecureKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebSecureKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
