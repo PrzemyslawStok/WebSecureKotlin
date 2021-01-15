5ff52ce7c5f82bd464f5967b379f49fe07e857a8
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-15 19:23:05 +0100

    cd..

--- src/main/kotlin/com/example/WebSecureKotlin/SimpleController.kt
+++ src/main/kotlin/com/example/WebSecureKotlin/SimpleController.kt
@@ -3,6 +3,7 @@ package com.example.WebSecureKotlin
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
@@ -13,7 +14,8 @@ class SimpleController {
         return "home"
     }
     @RequestMapping("/home")
-    fun home():String{
+    fun home(model: Model):String{
+        model.addAttribute("name","Przemysław")
         return "home"
     }
 
@@ -24,9 +26,12 @@ class SimpleController {
 
     @RequestMapping("info")
     @ResponseBody
-    fun info():String{
+    fun info(model: Model):String{
+
         val authentication: Authentication = SecurityContextHolder.getContext().authentication
+
         val name = authentication.name
+        val principal = authentication.principal
 
         return "Info: $name"
     }
